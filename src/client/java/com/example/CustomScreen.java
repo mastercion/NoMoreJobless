ad2e8dce307a02d59a4a467d30dd0ed0eff0c269
Andrej Wilhelm <andrejw39v2@gmail.com>
2024-02-09 21:38:34 +0100

    NoMoreJobless: Show Cords too and be clickable
    Added: Let the Shown SavedPos be clickable to teleport
    
    Signed-off-by: mastercion <andrejw39v2@gmail.com>
    Signed-off-by: Andrej Wilhelm <andrejw39v2@gmail.com>

--- src/client/java/com/example/CustomScreen.java
+++ src/client/java/com/example/CustomScreen.java
@@ -10,9 +10,13 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.client.gui.tooltip.Tooltip;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,10 +25,12 @@ import java.util.List;
 public class CustomScreen extends Screen implements CustomScreenInf {
     private ButtonWidget button1;
     private ButtonWidget button2;
+    private ButtonWidget button3;
 
     protected CustomScreen(MutableText customUi) {
         super(Text.literal("Custom UI"));
     }
+
 
     @Override
     protected void init() {
@@ -32,26 +38,36 @@ public class CustomScreen extends Screen implements CustomScreenInf {
         // Calculate button positions dynamically based on screen width
         int startY = this.height / 4 + 48; // Example starting Y position
 
+
         // Inside your CustomScreen class' init method where you define button1
         button1 = ButtonWidget.builder(Text.translatable("button.customscreen.buttonone"), button -> {
-                    // Action for Button 1
                     MinecraftClient client = MinecraftClient.getInstance();
-                    if (client.world != null) {
-                        for (Entity entity : client.world.getEntities()) {
+                    if (client.world != null && client.player != null) {
+                        client.world.getEntities().forEach(entity -> {
                             if (entity instanceof ArmorStandEntity) {
                                 ArmorStandEntity armorStand = (ArmorStandEntity) entity;
-                                if (armorStand.hasCustomName()) {
-                                    Text customName = armorStand.getCustomName();
-                                    if (customName != null && customName.getString().startsWith("SavedPos_")) {
-                                        client.player.sendMessage(Text.literal(customName.getString()), false);
-                                    }
+                                if (armorStand.hasCustomName() && armorStand.getCustomName().getString().startsWith("SavedPos_")) {
+                                    // Fetching the ArmorStand's position
+                                    BlockPos pos = armorStand.getBlockPos();
+                                    String command = "/tp @s " + pos.getX() + " " + pos.getY() + " " + pos.getZ();
+                                    String message = String.format("Click to teleport to %s at X:%d Y:%d Z:%d",
+                                            armorStand.getCustomName().getString(),
+                                            pos.getX(), pos.getY(), pos.getZ());
+
+                                    // Create a text component with click event for teleportation
+                                    Text teleportText = Text.literal(message).styled(style ->
+                                            style.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, command))
+                                                    .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("Teleport to ArmorStand"))));
+
+                                    // Sending the clickable message to the player
+                                    client.player.sendMessage(teleportText, false);
                                 }
                             }
-                        }
+                        });
                     }
                 })
-                .position(this.width / 2 - 205, 20) // Adjust position as needed
-                .dimensions(200, 20, 200, 20) // Adjust dimensions as needed
+                .position(this.width / 2 - 205, 20)
+                .dimensions(200, 20,200,20)
                 .tooltip(Tooltip.of(Text.literal("Prints all named armor stands starting with 'savedpos_'")))
                 .build();
 
@@ -89,13 +105,28 @@ public class CustomScreen extends Screen implements CustomScreenInf {
     }
 
     @Override
-    public void render(DrawContext drawContext, MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        this.renderBackground(drawContext); // Render the background of the screen
-        super.render(drawContext, mouseX, mouseY, delta); // Render buttons and other components
-        drawCenteredText(matrices, this.textRenderer, this.title, this.width / 2, 15, 0xFFFFFF); // Draw the screen title
+    public void renderBackground(DrawContext drawContext) {
+        assert this.client != null;
+        if (this.client.world != null) {
+            drawContext.fillGradient(0, 0, this.width, this.height, -1072689136, -804253680);
+        } else {
+            this.renderBackgroundTexture(drawContext);
+        }
     }
 
-    private void drawCenteredText(MatrixStack matrices, TextRenderer textRenderer, Text title, int i, int i1, int i2) {
+
+    public void renderBackgroundTexture(DrawContext context) {
+        context.setShaderColor(0.25F, 0.25F, 0.25F, 1.0F);
+        boolean i = true;
+        context.drawTexture(OPTIONS_BACKGROUND_TEXTURE, 0, 0, 0, 0.0F, 0.0F, this.width, this.height, 32, 32);
+        context.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+    }
+
+    @Override
+    public void render(DrawContext drawContext, MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        this.renderBackground(drawContext);
+        drawContext.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 15, 16777215);
+        super.render(drawContext, mouseX, mouseY, delta);
     }
 }
 
--- src/client/java/com/example/CustomScreenInf.java
+++ src/client/java/com/example/CustomScreenInf.java
@@ -4,5 +4,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.util.math.MatrixStack;
 
 public interface CustomScreenInf {
+    void renderBackground(DrawContext drawContext);
+
     void render(DrawContext drawContext, MatrixStack matrices, int mouseX, int mouseY, float delta);
 }
